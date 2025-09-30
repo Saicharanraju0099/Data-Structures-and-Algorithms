0409eeb60e38a57dc3a6238986b7486c15bc3424
Saicharan <rramu6552@gmail.com>
2025-09-30 22:09:28 +0530

    2-D Array

---
+++ 2 - D ARRAYS/2-D_Matrix_Array.py
@@ -0,0 +1,12 @@
+## 2D Matrix Array
+def matrix_array(arr,m,n):
+    for i in range(m):
+        for j in range(n):
+            print(arr[i][j],end=" | ")
+        print("\n")
+
+
+arr = [[1,2,3],[4,5,6],[7,8,9]]
+m = n = 3
+print("2D Matrix Array")
+print(matrix_array(arr,m,n))
